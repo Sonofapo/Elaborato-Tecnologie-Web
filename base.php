a1d1e9849fe006153b58642da1e4bb8fb95488f7
Sonofapo <oinotna842@gmail.com>
2022-01-23 10:51:37 +0100

    Implementazione login base

--- base.php
+++ base.php
@@ -1,1 +1,25 @@
-<!-- incredibile pagina di presentazione sito -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+	<!-- variabili varie per capire dove ci troviamo aka login ecc. -->
+	<title>UniBonsai - Login</title>
+	<link rel="icon" href="./img/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="./style/style.css" type="text/css" />
+</head>
+<body>
+	<header>
+		<img src="./img/logo.png"/>
+		<h1>UniBonsai</h1>
+		<br>
+		<a href="index.php?action=login">Effettua login</a>
+		<a href="index.php?action=logout">Effettua logout</a>
+	</header>
+	<main>
+		<div><?php echo $content ?></div>
+		<?php echo isset($_SESSION["uid"]) ? "LOGGATO" : "NON LOGGATO" ?>
+	</main>
+	<footer>
+		<a href="index.php">Torna alla Home</a>
+	</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,35 @@
-<?php 
-    //controllo login 
-    //1. c'e' una sessione attiva
-    //2. no login default catalogo
-    //3.effettuato accesso (controllo) ... vedi punto 1.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        //controlli infiniti della merd vomitevoleau
-        echo $_POST["username"];
-    } else {
-        require "base.php";
-    }
+<?php
+
+	session_start();
+
+	$action = $_REQUEST["action"] ?? "catalogo";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST" && $action == "login") {
+		$usr = $_POST["username"];
+		$psw = $_POST["password"];
+		$_SESSION["uid"] = $usr;
+		header("Location: index.php");
+	} else if (isset($_SESSION["uid"])) {
+		echo "SESSIONE ACCETTATA";
+	}
+
+	switch ($action) {
+		case "catalogo":
+			$page = "base.php";
+			$content = "PAGINA BELLISSIMA PRINCIPALISSIMA DE LA MÈR";
+			break;
+		case "login":
+		case "subscribe":
+			$page = "login.php";
+			$isLogin = $action == "login" ? true : false;
+			break;
+		case "logout":
+			session_unset();
+			session_destroy();
+			header("Location: index.php");
+			break;
+	}
+
+	require $page;
+
 ?>
