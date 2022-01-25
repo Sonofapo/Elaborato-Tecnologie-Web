2d98eff68152038d76e3a6d55e6076009e379d4d
Sonofapo <oinotna842@gmail.com>
2022-01-25 10:49:17 +0100

    Modifica header

--- base.php
+++ base.php
@@ -7,40 +7,38 @@
 	<link rel="icon" href="./img/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="./style/style.css" type="text/css" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 <body class="bg-light">
 	<header>
-		<!-- <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:OliveDrab">
-			<a class="navbar-brand" href="index.php">
-				<img id="logo-img" src="./img/logo.png" alt="" /><span>UniBonsai</span>
-			</a>
-			<div class="collapse navbar-collapse ml-5" id="nav-options">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="nav-item">
-						<a href="index.php?action=login" class="nav-link">Effettua login</a>
-					</li>
-					<li class="nav-item">
-						<a href="index.php?action=logout" class="nav-link">Effettua logout</a>
-					</li>
-				</ul>
-			</div>
-		</nav> -->
 		<nav class="navbar navbar-expand-lg navbar-dark">
 			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="index.php">
-						<img id="logo-img" src="./img/logo.png" alt="" /><span>UniBonsai</span>
-					</a>
+				<div class="col-5 navbar-collapse collapse">
+					<div class="navbar-nav">
+						<a class="me-auto" href="index.php">
+							<img id="logo-img" src="./img/logo.png" alt="" />
+						</a>
+					</div>
 				</div>
-				<ul class="nav navbar-nav navbar-right">
-					<li class="nav-item"><a href="notifiche.php"><i class='fas fa-bell'></i> Notifiche </a></li>
-					<li class="nav-item"><a href="profilo.php"><i class='fas fa-user-alt'></i> Profilo </a></li>
-					<li class="nav-item"><a href="#"><i class="fas fa-sign-out-alt"></i></span> Logout </a></li>
-					<!-- <li class="nav-item"><a href="#"><i class="fas fa-sign-in-alt"></i></span> Login</a></li>      check already logged in-->
-				</ul>
+				<a class="col-2 navbar-brand" href="#">UniBonsai</a>
+				<div class="col-5 navbar-collapse collapse" id="nav-opt">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="notifiche.php"><i class='fa fa-bell'></i> Notifiche</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="profilo.php"><i class='fa fa-user'></i> Profilo</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><i class="fa fa-sign-out"></i> Logout</a>
+						</li>
+					</ul>
+				</div>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-opt" aria-expanded="false" aria-controls="nav-opt">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 			</div>
 		</nav>
 	</header>
@@ -49,7 +47,7 @@
 		<?php echo "UTENTE: " . ($db->getUserById($_SESSION["uid"] ?? -1) ?: "NON LOGGATO") ?>
 	</main>
 	<footer>
-		<a href="index.php"><i class='fas fa-home'></i> Torna alla Home</a>
+		<a href="index.php"><i class='fa fa-home'></i> Torna alla Home</a>
 	</footer>
 </body>
 </html>
