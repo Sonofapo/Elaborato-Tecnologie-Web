dcdb3a08c4710d684f9ef2bb3753f00d2fd132bc
Sonofapo <oinotna842@gmail.com>
2022-02-02 08:33:09 +0100

    Cambio presentazione e correzione prezzo

--- src/catalogo/cart.php
+++ src/catalogo/cart.php
@@ -11,8 +11,10 @@
 				</div>
 				<div class="product-info">
 					<p class="product-name"><?php echo ucfirst($product["name"]) ?></p>
-					<p>Prezzo: <?php echo $product["price"] ?>&euro;</p>
-					<p>Quantità: <?php echo $product["quantity"] ?></p>
+					<p>
+						Quantità: <?php echo $product["quantity"] ?> -
+						Prezzo: <?php echo $product["price"] * $product["quantity"] ?>&euro;
+					</p>
 					<button class="remove-from-cart btn btn-danger" id="prod-<?php echo $product["id"] ?>">
 						Rimuovi
 					</button>
