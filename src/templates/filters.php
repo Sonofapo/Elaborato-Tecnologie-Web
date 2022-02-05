eb87254fc360b5ed59704f8642ee43d51820e3e8
Sonofapo <oinotna842@gmail.com>
2022-02-05 08:34:41 +0100

    Correzione numerazioni id per univocità

--- src/templates/filters.php
+++ src/templates/filters.php
@@ -1,3 +1,4 @@
+<?php $c = 0 ?>
 <?php foreach ($vars["filters"] as $name => $f): ?>
 <section>
 	<h2 class="header">
@@ -7,7 +8,6 @@
 	</h2>
 	<div id="search-<?php echo $name ?>" class="collapse" data-bs-parent="#accordion">
 		<ul class="body">
-			<?php $c = 0 ?>
 			<?php foreach ($f["values"] as $value): ?>
 			<li>
 				<input form="search-f" type="checkbox" id="f-<?php echo $c ?>" name="<?php echo $name ?>[]"
